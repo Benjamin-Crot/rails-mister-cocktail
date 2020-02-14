1081be1fdca3a95e29ce6f134d0bc2c50f5c63cf
Benjamin Crot <benjamin.crot@gmail.com>
2020-02-14 11:00:57 +0100

    done

---
+++ db/migrate/20200213133432_add_column_image.rb
@@ -0,0 +1,4 @@
+class AddColumnImage < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+Ingredient.destroy_all
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+ingredient_array = user['drinks']
+
+ingredient_array.each do |i|
+  ingredient = Ingredient.new(name: i['strIngredient1'])
+  ingredient.save!
+end
+puts 'Done'
+
+Cocktail.destroy_all
+
+Cocktail.create(name: 'Mojito')
+Cocktail.create(name: 'Moscow Mule')
+Cocktail.create(name: 'Piña Colada')
+Cocktail.create(name: 'Ti Punch')
+Cocktail.create(name: 'Margarita')
+Cocktail.create(name: 'Gin Tonic')
+Cocktail.create(name: 'Tequila Sunrise')
+Cocktail.create(name: 'Whisky Sour')
